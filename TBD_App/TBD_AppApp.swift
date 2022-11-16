34782191eeda8f4b6d231a7cbf7bef1865244303
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2022-11-16 17:45:56 +0100

    Initial Commit

---
+++ TBD_App/TBD_AppApp.swift
@@ -0,0 +1,20 @@
+//
+//  TBD_AppApp.swift
+//  TBD_App
+//
+//  Created by Many Kaufmann on 16.11.22.
+//
+
+import SwiftUI
+
+@main
+struct TBD_AppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
