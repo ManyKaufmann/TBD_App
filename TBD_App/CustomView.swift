27ea4a4e7bea35a15d161df45e0aab5235b06df9
Nizam <34688063+Nizii@users.noreply.github.com>
2022-11-18 11:42:23 +0100

    Star

---
+++ TBD_App/CustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomView.swift
+//  TBD_App
+//
+//  Created by HSLU-N0004388 on 18.11.22.
+//
+
+import SwiftUI
+
+struct CustomView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CustomView_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomView()
+    }
+}
--- TBD_App/StartView.swift
+++ TBD_App/StartView.swift
@@ -13,22 +13,27 @@ struct StartView: View {
         NavigationView {
             VStack() {
                 Text("Your Name")
-                    .font(.callout)
+                    .font(.title)
                 Spacer().frame(height: 0)
                 TextField("", text: $userName)
-                    .frame(width: 300.0, height: 40.0)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .border(Color.blue)
+                    .frame(width: 300.0, height: 60.0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 6)
+                            .stroke(.gray.opacity(0.5), lineWidth: 2)
+                    )
                     .fixedSize()
 
-                Spacer().frame(height: 150)
+                Spacer().frame(height: 120)
                 Button(){}label: {
                     Text("Start")
                 }
-                .frame(width: 100.0, height: 60.0)
-                    .background(Color.blue)
-                    .foregroundColor(Color.white)
-                    .cornerRadius(10)
+                .frame(width: 140.0, height: 40.0)
+                    .foregroundColor(Color.blue)
+                    .cornerRadius(0)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(.blue.opacity(0.8), lineWidth: 1)
+                    )
             }
         }
     }
