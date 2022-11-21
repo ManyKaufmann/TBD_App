26930e219800d1d1661241377e618dfbfe3be6e8
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2022-11-21 20:46:06 +0100

    add Placeholder for OverView

--- TBD_App/OverView.swift
+++ TBD_App/OverView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct OverView: View {
+    @State private var showingSheet = false
+    //Implement Array with separate View
+    private var data: [Int] = Array(1...4)
+    private let adaptiveColumns = [GridItem(.adaptive(minimum: 170))]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView{
+            LazyVGrid(columns: adaptiveColumns, spacing: 20){
+                ForEach (data, id: \.self) {
+                    number in ZStack {
+                        Rectangle()
+                            .frame(width: 170, height: 170)
+                            .foregroundColor(.pink)
+                            .cornerRadius(30)
+                        Button("Show Activity") {
+                            showingSheet.toggle()
+                        }
+                        .sheet(isPresented: $showingSheet) {
+                            DetailView()
+                        }
+                            .foregroundColor(.white)
+                            .font(.system(size: 24, weight: .medium, design: .rounded))
+                    }
+                }
+            }
+            .padding()
+            .navigationTitle("Hello NAME")
+            
+                
+                
+            
+        }
     }
 }
 
