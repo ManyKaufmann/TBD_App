51ecc89c08bdba695b77d8ac7aa75e3440a7c7ee
ManyKaufmann <manynouk.kaufmann@stud.hslu.ch>
2022-11-21 19:27:14 +0100

    create OverView and DetailView

---
+++ TBD_App/OverView.swift
@@ -0,0 +1,20 @@
+//
+//  OverView.swift
+//  TBD_App
+//
+//  Created by Many Kaufmann on 21.11.22.
+//
+
+import SwiftUI
+
+struct OverView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OverView_Previews: PreviewProvider {
+    static var previews: some View {
+        OverView()
+    }
+}
